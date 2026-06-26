8930522e121ab621ee9f042c60f52530f9f2b58b
Anna <annaknyseva3@gmail.com>
2026-06-26 09:22:14 +0300

    Update db_manager.py

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -1,1 +1,16 @@
+"""
+Управление базой данных.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database.models import Base
+from app.config import Config
 
+engine = create_engine(f'sqlite:///{Config.DB_PATH}', echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_session():
+    return SessionLocal()
